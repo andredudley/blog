4b5eb6f1db81ca66fa91126b6c96f36b2f5808e3
andredudley <andredudley31@gmail.com>
2026-02-10 12:50:17 -0500

    potential fix for bug in log dates

--- .eleventy.js
+++ .eleventy.js
@@ -2,28 +2,20 @@ const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
 
-  // Add date filter for Nunjucks
-const { DateTime } = require("luxon");
-
-module.exports = function(eleventyConfig) {
+  // Date filter (fixes the one-day-off bug)
   eleventyConfig.addFilter("postDate", (dateObj, format = "MMMM d, yyyy") => {
     return DateTime
-      .fromJSDate(dateObj, { zone: "utc" }) // treat input as UTC
-      .toLocal()                            // converts to local time
+      .fromJSDate(dateObj, { zone: "utc" })
+      .toLocal()
       .toFormat(format);
   });
-};
 
-  // Copy existing static site files through to log
-  eleventyConfig.addPassthroughCopy("index.html");
-  eleventyConfig.addPassthroughCopy("css");
-  eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addPassthroughCopy("scripts");
-  eleventyConfig.addPassthroughCopy("pages"); // if other static pages exist
+
 
   return {
     dir: {
-      input: "log-src",   // ONLY Eleventy content lives here
+      input: "log-src",
+      includes: "_includes",
       output: "log"
     }
   };
