09a1664c6905e1ae3aa69b8301fccbed88f0dddd
andredudley <andredudley31@gmail.com>
2026-02-10 14:54:10 -0500

    Revert "more fixes for log dates"
    This reverts commit 40a0075f151aea93a0b9a4cbc6f7ce366a5efae8.

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 const { DateTime } = require("luxon");
 
 module.exports = function(eleventyConfig) {
+<<<<<<< HEAD
 
   // Date filter (safe, no timezone shift for displayDate)
   eleventyConfig.addFilter("postDate", (dateInput, format = "MMMM d, yyyy") => {
@@ -15,11 +16,23 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("scripts");
   eleventyConfig.addPassthroughCopy("pages"); // if other static pages exist
+=======
+  eleventyConfig.addFilter("postDate", (dateObj, format = "MMMM d, yyyy") => {
+    return DateTime
+      .fromJSDate(dateObj, { zone: "utc" }) // treat input as UTC
+      .toLocal()                            // convert to est
+      .toFormat(format);
+  });
+};
+
+
+
+>>>>>>> parent of 40a0075 (more fixes for log dates)
 
   return {
     dir: {
-      input: "log-src",   // ONLY Eleventy content lives here
+      input: "log-src",
+      includes: "_includes",
       output: "log"
     }
   };
-};
