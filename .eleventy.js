5d9b19782844e0b8ad797edd4e0c416c85c47227
andredudley <andredudley31@gmail.com>
2026-02-10 12:53:15 -0500

    comments

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("postDate", (dateObj, format = "MMMM d, yyyy") => {
     return DateTime
       .fromJSDate(dateObj, { zone: "utc" }) // treat input as UTC
-      .toLocal()                            // convert to your local time
+      .toLocal()                            // convert to est
       .toFormat(format);
   });
 };
